b2d3ba4f7ae62df41de7ceb855f31e5056eaac77
mbinshahzad <mgreat1337@gmail.com>
2016-04-16 19:18:32 +0500

    Update atom.py

--- atom.py
+++ atom.py
@@ -3,11 +3,12 @@
 import mechanize
 import time
 import argparse
+import threading
 
 parser = argparse.ArgumentParser(description="Atom - This a XSS finder coded in Python by Muhammad Shahzad, This was coded by him for automating his task and not launching it or make use of it to market himself like others are doing. This tool spiders hundreds of pages, which may take a little time as well - After spidering then it searches for forms present in those pages and then check those pages by filling those forms and then checking if our input is reflected back in the HTML document. This tool should be used for legal purposes for example testing your own sites/applications as testing others without permission is considered illegal. ")
 parser.add_argument('-t', type=str, help="Define the target URL - Make sure its a valid one running on HTTP protocol.", required=True)
+parser.add_argument('-p', type=str, help="Define path to a file containing custom payloads to be injected.", required=False)
 cmdargs = parser.parse_args()
-
 
 class xssfinder:
 
@@ -34,10 +35,11 @@ class xssfinder:
 
 		self.count = 0
 		self.vulnerable = 0
+		self.payload = 0
 
 	def spider(self):
 		time_sp = time.time()
-		print "Started spidering"
+		print "\n[+] Started spidering"
 		try:
 
 			for link in self.br.links():
@@ -78,14 +80,15 @@ class xssfinder:
 		finally:
 
 			for l in self.links:
-				print l + '\n'
-		print "\n Finished Spidering in: %s seconds" % str(time.time() - time_sp)
+				file_spider = open('atom-spidering.txt','a')
+				file_spider.write(l +'\n')
+		print "\n[+] File atom-spidering.txt saved!"
+		print "\n[-] Finished Spidering in: %s seconds" % str(time.time() - time_sp)
 
 	def xssfind(self):
 
 
-		time_f = time.time()
-		print "Started searching for forms."
+		print "\n[+]Started searching for forms."
 
 		for l in self.links:
 			try:
@@ -107,7 +110,6 @@ class xssfinder:
 				pass
 			if self.count > 0:
 
-				time_form = time.time()
 				try:
 
 					param = list(self.br.forms())[0]
@@ -146,7 +148,7 @@ class xssfinder:
 
 					try:
 
-						if '<img/src=x onerror=prompt(1)>' in self.br.response().read():
+						if '<svg/onload=prompt(1)>' in self.br.response().read():
 
 							print l + ' is vulnerble' 
 
@@ -171,8 +173,108 @@ class xssfinder:
 		else:
 
 			print "No vulnerale form."
-		print "Completed testing forms in: %s seconds" % str(time.time() - time_form)
 
-xss = xssfinder(cmdargs.t)
-xss.spider()
-xss.xssfind()
+	def xssfind_payload(self):
+		print "\n[+] Started searching for forms."
+		try:
+			f = open(cmdargs.p)
+		except:
+			pass
+		for line in f:
+			self.payload += 1
+
+			for l in self.links:
+				try:
+
+					self.br.open(l)
+
+				except:
+
+					pass
+
+				try:
+
+					for form in self.br.forms():
+
+						self.count += 1
+
+				except:
+
+					pass
+				if self.count > 0:
+					
+					try:
+
+						param = list(self.br.forms())[0]
+
+					except:
+
+						pass
+
+					try:
+
+						self.br.select_form(nr=0)
+
+					except:
+
+						pass
+					for p in param.controls :
+
+						try:
+
+							if 'TextControl' in p:
+
+								self.br.form[str(p.name)] = line
+
+						except:
+
+							pass
+
+						try:
+
+							self.br.submit()
+
+						except:
+
+							pass
+						try:
+							if line in self.br.response().read():
+								print l + " is vulnerable"
+								self.vulnerable +=1
+						except:
+							pass
+
+						try:
+
+							self.br.back()
+
+						except:
+
+							pass
+
+		if self.vulnerable > 0:
+
+			print "\n[+]Vulnerable forms: " + str(self.vulnerable)
+
+		else:
+
+			print "\n[+]No vulnerale form."
+		if self.payload > 0:
+			print "\n[+]Custom Payload loaded: %s" % self.payload
+
+
+class myThread(threading.Thread):
+
+	def __init__(self):
+		threading.Thread.__init__(self)
+
+	def run(self):
+		xss = xssfinder(cmdargs.t)
+		xss.spider()
+		if cmdargs.p :
+			xss.xssfind_payload()
+		else:
+			xss.xssfind()
+
+thread = myThread()
+thread.start()
